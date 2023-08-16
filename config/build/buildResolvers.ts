b1d451006d7def24a398774b29c772bb3cd9200e
Arthur Osipov <osartie@gmail.com>
2023-08-16 09:51:25 +0300

    Добавлен React и настрое лоадер для SCSS

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -3,6 +3,6 @@ import { ResolveOptions } from "webpack";
 export function buildResolvers(): ResolveOptions {
 
     return {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: ['.tsx', '.ts', '.js', '.scss'],
     }
 }
