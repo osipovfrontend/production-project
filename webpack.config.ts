b8c9e7cb4f994b45ca2df8ac1380a089d7c6d9d5
Arthur Osipov <osartie@gmail.com>
2023-08-24 14:16:53 +0300

    Добавлено CSS Modules

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,29 +1,28 @@
-// т.к. в разных ОС работа пути работают по разному, лучше использовать специальные инструменты
-// webpack работает в среде nodeJS и нам доступен модуль path
-// функция resolve позволяет "склеивать" участки пути
 import webpack from 'webpack';
-import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
 import path from "path";
 
+
 export default (env: BuildEnv) => {
-  const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    build: path.resolve(__dirname, 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html')
-  }
-  
-  const mode = env.mode || 'development';
-  const PORT = env.port || 3000;
 
-  const isDev = mode === 'development';
-  
-  const config: webpack.Configuration = buildWebpackConfig({
-    mode,
-    paths,
-    isDev,
-    port: PORT,
-  });
 
-  return config;
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
+
+    const mode = env.mode || 'development';
+    const isDev = mode === 'development';
+    const PORT = env.port || 3000;
+
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port: PORT,
+    })
+
+    return config;
 };
