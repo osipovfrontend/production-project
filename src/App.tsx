5f9fc78a7016dd4d2374f5b5c18094a3ae3b6368
Arthur Osipov <osartie@gmail.com>
2023-08-29 18:07:34 +0300

    add: themisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes, Link } from 'react-router-dom';
 import Counter from "./components/Counter";
-import './index.scss';
+import './styles/index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { Suspense } from 'react';
+import { Suspense, useContext, useState } from 'react';
+import { Theme, ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './styles/themes/useTheme';
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();    
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading</div>}>
