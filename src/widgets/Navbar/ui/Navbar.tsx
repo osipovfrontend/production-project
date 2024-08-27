26348917ab3032e94cdb9d71488955d19d93bdd3
Arthur Osipov <osartie@gmail.com>
2024-08-27 14:51:57 +0500

    Добавил Sidebar и перенес в него переключатель тем

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import {ThemeSwitcher} from 'shared/ui/ThemeSwitcher';
 
 interface NavbarProps {
   className?: string;
@@ -10,8 +9,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
 
   return (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-      <ThemeSwitcher />
+    <div className={classNames(cls.Navbar, {}, [className])}>      
       <div className={cls.links}>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
