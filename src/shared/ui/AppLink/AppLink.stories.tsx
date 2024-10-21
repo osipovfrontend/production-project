26ced08977d5c671b854574e4a300c42431383b9
Arthur Osipov <osartie@gmail.com>
2024-10-21 19:46:27 +0500

    Поправил ошибки с версией TS

--- src/shared/ui/AppLink/AppLink.stories.tsx
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-import { Button, ThemeButton } from './Button';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { AppLink, AppLinkTheme } from './AppLink';
--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -20,7 +20,7 @@ const meta: Meta<typeof ThemeSwitcher> = {
         // backgroundColor: { control: 'color' },
     },
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-    args: { to: '/' },
+    args: {},
 };
 
 export default meta;
